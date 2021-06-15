0f7d13690d6f90bd07a1e27e084bdb6749861c96
avalontm <avalontm21@gmail.com>
2021-06-15 15:38:40 -0700

    test

---
+++ Console_Team/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Console_Team
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
